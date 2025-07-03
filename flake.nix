cb4360d98aa745168f82569b70c154599413ade1
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-07-03 09:05:46 +0000

    astro template

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      buildInputs = [
+        nixpkgs.legacyPackages.x86_64-linux.nodejs
+        nixpkgs.legacyPackages.x86_64-linux.pnpm
+      ];
+    };
+  };
+}
